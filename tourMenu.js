2741dd6069efa6a687f63c410f737ed24aad828e
Justin Fuller <justin.93x@gmail.com>
2023-04-25 23:03:49 -0500

    Updated Dropdown menu/load tour button

--- tourMenu.js
+++ tourMenu.js
@@ -1,39 +1,69 @@
 const wrapper = document.querySelector(".wrapper"),
-selectBtn = wrapper.querySelector(".select-btn"),
-searchInp = wrapper.querySelector("input"),
-options = wrapper.querySelector(".options");
+  selectBtn = wrapper.querySelector(".select-btn"),
+  searchInp = wrapper.querySelector("input"),
+  options = wrapper.querySelector(".options");
 
-let countries = ["Tree Tour", "Legume Family", "Grass Family", "Edible", "Medicinal", "Shakespeare Tree Tour",
-                 "Butterflies", "Birds", "Flowering Trees of WSU", "Cultivar Garden", "Bioswale #3", 
-                 "Bioswale #1 & #2", "Bioswale #4", "Bioswale #5", "Younger Courtyard"];
+const loadTourDiv = document.getElementById("loadButtonDiv");
+
+let countries = [
+  "Tree Tour",
+  "Legume Family",
+  "Grass Family",
+  "Edible",
+  "Medicinal",
+  "Shakespeare Tree Tour",
+  "Butterflies",
+  "Birds",
+  "Flowering Trees of WSU",
+  "Cultivar Garden",
+  "Bioswale #3",
+  "Bioswale #1 & #2",
+  "Bioswale #4",
+  "Bioswale #5",
+  "Younger Courtyard",
+  "Kai Selection",
+];
 
 function addCountry(selectedCountry) {
-    options.innerHTML = "";
-    countries.forEach(country => {
-        let isSelected = country == selectedCountry ? "selected" : "";
-        let li = `<li onclick="updateName(this)" class="${isSelected}">${country}</li>`;
-        options.insertAdjacentHTML("beforeend", li);
-    });
+  options.innerHTML = "";
+  countries.forEach((country) => {
+    let isSelected = country == selectedCountry ? "selected" : "";
+    let li = `<li onclick="updateName(this)" class="${isSelected}">${country}</li>`;
+    options.insertAdjacentHTML("beforeend", li);
+  });
 }
 addCountry();
 
 function updateName(selectedLi) {
-    searchInp.value = "";
-    addCountry(selectedLi.innerText);
-    wrapper.classList.remove("active");
-    selectBtn.firstElementChild.innerText = selectedLi.innerText;
+  searchInp.value = "";
+  addCountry(selectedLi.innerText);
+  wrapper.classList.remove("active");
+  selectBtn.firstElementChild.innerText = selectedLi.innerText;
+  let test = `
+  <div id='loadButtonDiv'>
+    <a href="treeFinder.html"h>
+    <button class="loadTourButton">Load Tour</button>
+    </a>
+  </div>`;
+  loadTourDiv.innerHTML = test;
 }
 
 searchInp.addEventListener("keyup", () => {
-    let arr = [];
-    let searchWord = searchInp.value.toLowerCase();
-    arr = countries.filter(data => {
-        return data.toLowerCase().startsWith(searchWord);
-    }).map(data => {
-        let isSelected = data == selectBtn.firstElementChild.innerText ? "selected" : "";
-        return `<li onclick="updateName(this)" class="${isSelected}">${data}</li>`;
-    }).join("");
-    options.innerHTML = arr ? arr : `<p style="margin-top: 10px;">Oops! Tree tour not found</p>`;
+  let arr = [];
+  let searchWord = searchInp.value.toLowerCase();
+  arr = countries
+    .filter((data) => {
+      return data.toLowerCase().startsWith(searchWord);
+    })
+    .map((data) => {
+      let isSelected =
+        data == selectBtn.firstElementChild.innerText ? "selected" : "";
+      return `<li onclick="updateName(this)" class="${isSelected}">${data}</li>`;
+    })
+    .join("");
+  options.innerHTML = arr
+    ? arr
+    : `<p style="margin-top: 10px;">Oops! Tree tour not found</p>`;
 });
 
 selectBtn.addEventListener("click", () => wrapper.classList.toggle("active"));
