c508e6fc84909cc9048f06f5f179f81ed5499834
Justin Fuller <justin.93x@gmail.com>
2023-04-27 15:07:28 -0500

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -2,6 +2,7 @@ const theBody = document.querySelector("body");
 const openNav = document.querySelector(".menu-bar button");
 const closeNav = document.querySelector(".close-nav button");
 const Navbar = document.querySelector(".navbar");
+const map = document.getElementById("map");
 
 function bodyScroll() {
   if (Navbar.classList.contains("show")) {
@@ -14,7 +15,32 @@ function bodyScroll() {
 function showHide() {
   Navbar.classList.toggle("show");
   bodyScroll();
+  hideMap();
+}
+
+function showHide1() {
+  Navbar.classList.toggle("show");
+  bodyScroll();
+  showMap();
+}
+
+function showMap() {
+  let html = `
+  <iframe
+    frameborder="0"
+    style="border: 2px solid black"
+    referrerpolicy="no-referrer-when-downgrade"
+    src="https://www.google.com/maps/embed/v1/view?key=AIzaSyC2kQfuFSpOvWEOmOPqqWpeOGD91kNLQIg&center=44.047546172925074,-91.64307448064426&zoom=16.5"
+    allow="fullscreen"
+  >
+  </iframe>
+`;
+  map.innerHTML = html;
+}
+
+function hideMap() {
+  map.innerHTML = "";
 }
 
 openNav.onclick = showHide;
-closeNav.onclick = showHide;
+closeNav.onclick = showHide1;
